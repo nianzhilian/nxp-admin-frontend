3c0d759a42ccb04f7082eaac57557c0be0fb8a60
nianzhilian <776440949@qq.com>
2020-12-29 18:07:12 +0800

    Update App.js

--- src1/App.js
+++ src1/App.js
@@ -1,115 +1,123 @@
-
 import React, { Suspense, PureComponent } from "react";
 import { Route, Link, Switch, Redirect } from "react-router-dom";
 const Login = React.lazy(() => import("./components/Login/index"));
 const Layout = React.lazy(() => import("./components/Layout/index"));
 const Home = React.lazy(() => import("./components/Home/index"));
 const NoPage = React.lazy(() => import("./components/NoPage/index"));
+const Sys = () => <div>系统管理</div>;
 const isAuth = () => {
-    let token = localStorage.getItem("token");
-    // return token ? true : false;
-    return true
+  let token = localStorage.getItem("token");
+  return token ? true : false;
 };
 
 const routes = [
-    {
-        path: "/login",
-        component: Login,
+  {
+    path: "/home",
+    name: "首页",
+    component: Layout,
+    redirect: "/home/index",
+    isAuth: isAuth(),
+    exact: false,
+    children: [
+      {
+        path: "/index",
+        component: Home,
         hidden: true,
-    },
-    {
-        path: "/home",
-        name: "首页",
-        component: Layout,
-        redirect: "/home/index",
-        isAuth: isAuth(),
-        children: [
-            {
-                path: "/home/index",
-                component: Home
-            },
-        ],
-    },
-    {
-        path:'/',
-        component: Layout,
-        redirect: "/home",
-        isAuth: isAuth()
-    }
-      
+      },
+      {
+        path: "/sys",
+        component: Sys,
+      },
+    ],
+  },
+  {
+    path: "/",
+    component: Layout,
+    redirect: "/home/index",
+    isAuth: isAuth(),
+    exact: true,
+  },
+  {
+    path: "/login",
+    component: Login,
+    hidden: true,
+    exact: true,
+  },
+  {
+    path: "*",
+    component: NoPage,
+    hidden:true
+  },
 ];
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-    render() {
-        let { token } = this.props;
-        return (
-            <Suspense fallback={<div>正在加载中</div>}>
-                <Switch>
-                    {routes.map((item, i) => {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    let { token } = this.props;
+    return (
+      <Suspense fallback={<div>正在加载中</div>}>
+        <Switch>
+          {routes.map((item, i) => {
+            return (
+              <Route
+                path={item.path}
+                key={i}
+                exact={item.exact}
+                render={(routerProps) => {
+                  if (item.isAuth) {
+                    if (item.children) {
+                      let location = routerProps.location;
+                      let isMatch = item.children.find((obj) => {
+                        return item.path + obj.path == location.pathname;
+                      });
+                      if (isMatch) {
                         return (
-                            <Route
-                                path={item.path}
-                                key={i}
-                                exact
-                                render={(routerProps) => {
-                                   
-                                    if (item.isAuth) {
-                                        alert(item.isAuth)
-                                        console
-                                        if (item.children) {
-                                            return (
-                                                <item.component {...routerProps}>
-                                                    <Switch>
-                                                        {item.children.map((route, j) => {
-                                                            return (
-                                                                <Route
-                                                                    path={route.path}
-                                                                    key={j}
-                                                                    
-                                                                    component={route.component}
-                                                                />
-                                                            );
-                                                        })}
-                                                        <Redirect from={item.path} to={item.redirect} />
-                                                        
-                                                    </Switch>
-                                                </item.component>
-                                            );
-                                        }else{
-                                            return <Redirect to={item.redirect} />
-                                        }
-                                    } else {
-                                        if(item.hidden){
-                                            return <item.component />;
-                                        }
-                                        return <Redirect to='login' />
-                                        if (item.hidden) {
-                                            alert('asdas')
-                                            return <item.component />;
-                                        } else {
-                                            alert('111')
-                                            // return (
-                                            //     <Redirect
-                                            //         to={{
-                                            //             pathname: "/login",
-                                            //             state: { from: routerProps.location },
-                                            //         }}
-                                            //     />
-                                            // );
-                                        }
-                                    }
-                                }}
-                            />
+                          <item.component {...routerProps}>
+                            <Switch>
+                              {item.children.map((route, j) => {
+                                return (
+                                  <Route
+                                    path={item.path + route.path}
+                                    key={j}
+                                    exact={route.exact}
+                                    component={(childRoutes) => {
+                                      return <route.component />;
+                                    }}
+                                  />
+                                );
+                              })}
+                            </Switch>
+                          </item.component>
                         );
-                    })}
-                    <Route component={NoPage} />
-                </Switch>
-            </Suspense>
-        );
-    }
+                      } else {
+                        return <Route path="*" component={NoPage} />;
+                      }
+                    } else {
+                      return <Redirect to={item.redirect} />;
+                    }
+                  } else {
+                    if (item.hidden) {
+                      return <item.component />;
+                    }
+                    return (
+                      <Redirect
+                        to={{
+                          pathname: "/login",
+                          state: { from: routerProps.location },
+                        }}
+                      />
+                    );
+                  }
+                }}
+              />
+            );
+          })}
+        </Switch>
+      </Suspense>
+    );
+  }
 }
 
 export default App;
